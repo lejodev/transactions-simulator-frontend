061eb5b5499fe56ac0abdd1e834900ec0cc42050
Alejandro <alejandro.rv97@gmail.com>
2025-12-18 14:21:49 -0500

    refactor: convert page from static to SPA

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 import { SHARED_MATERIAL_IMPORTS } from '../../shared/material/material.imports';
 import { UsersPicker } from "../transactions/components/users-picker/users-picker";
 import { User } from '../users/models/user.model';
 import { Services } from '../users/services/user.service';
 import { TransactionForm } from "../transactions/components/transaction-form/transaction-form";
-import { TransactionsList } from "../transactions/components/transactions-list/transactions-list";
 import { TransactionService } from '../transactions/services/transaction.service';
-import { Transaction } from '../transactions/models/transaction.model';
 
 @Component({
     selector: 'app-home',
@@ -17,8 +16,7 @@ import { Transaction } from '../transactions/models/transaction.model';
         CommonModule,
         SHARED_MATERIAL_IMPORTS,
         UsersPicker,
-        TransactionForm,
-        TransactionsList
+        TransactionForm
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
@@ -27,18 +25,16 @@ export class HomeComponent {
 
     users = signal<User[]>([]);
     selectedUser = signal<User | null>(null);
-    transactions = signal<Transaction[]>([]);
 
     constructor(
         private userService: Services,
-        private transactionService: TransactionService) { }
+        private transactionService: TransactionService,
+        private router: Router
+    ) { }
 
     ngOnInit() {
         this.userService.getUsers().subscribe(users => {
             this.users.set(users);
-        });
-        this.transactionService.getTransactions().subscribe(transactions => {
-            this.transactions.set(transactions);
         });
     }
 
@@ -53,14 +49,17 @@ export class HomeComponent {
                 amount: transaction.amount,
                 user: this.selectedUser()!
             });
-            this.transactionService.getTransactions().subscribe(transactions => {
-                this.transactions.set(transactions);
-            });
             this.selectedUser.set(null);
+            // Navigate to history page to show the new transaction
+            this.router.navigate(['/history']);
         }
         else {
             console.log("No user selected");
         }
+    }
+
+    navigateToHistory() {
+        this.router.navigate(['/history']);
     }
 
 }
