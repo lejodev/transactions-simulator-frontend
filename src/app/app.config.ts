474bf66ff8af406faa6529827ffff5e1a7a45794
Alejandro <alejandro.rv97@gmail.com>
2025-12-18 17:09:49 -0500

    feat: X frontend version header added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { versionHeaderInterceptor } from './core/interceptors/version-header.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([versionHeaderInterceptor]))
   ]
 };
